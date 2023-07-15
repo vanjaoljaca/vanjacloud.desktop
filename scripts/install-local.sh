032e40ae3aa4422c758efcb709efffc09dee9fbb
Vanja Oljaca <vanja.oljaca@me.com>
2023-07-14 22:33:54 -1000

    Remove .env from install
    import of keys.json will merge at build time

--- scripts/install-local.sh
+++ scripts/install-local.sh
@@ -2,6 +2,5 @@
 yarn make --arch=arm64 --platform=darwin
 pkill vanjacloudtop
 rm -rf ~/Applications/vanjacloudtop.app
-cp .env out/vanjacloudtop-darwin-arm64/vanjacloudtop.app/Contents/MacOS/
 cp -r out/vanjacloudtop-darwin-arm64/vanjacloudtop.app ~/Applications
 open ~/Applications/vanjacloudtop.app
